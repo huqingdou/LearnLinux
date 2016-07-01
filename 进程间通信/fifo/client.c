7716a598ae6c9d040428a259f9eb424fee1cef08
hm <15686175080@163.com>
2016-07-01 23:50:49 +0800

    fifo_client

---
+++ 进程间通信/fifo/client.c
@@ -0,0 +1,49 @@
+/*************************************************************************
+    > File Name: client.c
+    > Author: hm
+    > Mail: 15686175080@163.com 
+    > Created Time: Fri 01 Jul 2016 11:36:38 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<string.h>
+#include<fcntl.h>
+
+#define _PATH_ "./my_fifo"
+#define _SIZE_ 100
+
+int main()
+{
+	int ret = mkfifo(_PATH_, 0666 | S_IFIFO);
+	if(ret == -1)
+	{
+		printf("mkfifo error\n");
+		return 1;
+	}
+	int fd = open(_PATH_, O_WRONLY);
+	if(fd < 0)
+	{
+		printf("open error\n");
+	}
+	char buf[_SIZE_];
+	memset(buf, '\0', sizeof(buf));
+	while(1)
+	{
+		scanf("%s", buf);
+		int ret = write(fd, buf, strlen(buf) + 1);
+		if(ret < 0)
+		{
+			printf("write error\n");
+			break;
+		}
+		if(strncmp(buf, "quit", 4) == 0)
+		{
+			break;
+		}
+	}
+	close(fd);
+	return 0;
+}
