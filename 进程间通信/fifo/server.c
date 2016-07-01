39d4d110e58918c08f83c46af8caddfc71088568
hm <15686175080@163.com>
2016-07-01 23:51:43 +0800

    fifo_server

---
+++ 进程间通信/fifo/server.c
@@ -0,0 +1,43 @@
+/*************************************************************************
+    > File Name: server.c
+    > Author: hm
+    > Mail: 15686175080@163.com 
+    > Created Time: Fri 01 Jul 2016 11:29:36 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<string.h>
+
+#define _PATH_ "./my_fifo"
+#define _SIZE_ 100
+
+int main()
+{
+	int fd = open(_PATH_, O_RDONLY);
+	if(fd < 0)
+	{
+		printf("open file error!\n");
+		return 1;
+	}
+	char buf[_SIZE_];
+	memset(buf, '\0', sizeof(buf));
+	while(1)
+	{
+		int ret = read(fd, buf, sizeof(buf));
+		if(ret <= 0)
+		{
+			printf("read end or error!\n");
+			break;
+		}
+		printf("%s\n", buf);
+		if(strncmp(buf, "quit", 4) == 0)
+		{
+			break;
+		}
+	}
+	close(fd);
+	return 0;
+}
